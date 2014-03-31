e965ba05c56c5ea4a1f133466d3b96301f834320
Niels Søholm <nm@9la.dk>
2014-03-31 12:46:19 +0200

    Moved frequent pattern mining and clustering into individual methods

--- assignment/mdmi_assignment.py
+++ assignment/mdmi_assignment.py
@@ -91,72 +91,75 @@ for val in columns["uni_yrs"]:
 #
 ########################################
 
-# Count language frequencies
-langfreq = dict()
-for val in columns["progLangs"]:
-	for lang in val.split(','):
-		langl = lang.lower().lstrip()
-		if(langl in langfreq.keys()):
-			langfreq[langl] = langfreq[langl] + 1
-		else:
-			langfreq[langl] = 1
+def frequentPattern():
+	# Count language frequencies
+	langfreq = dict()
+	for val in columns["progLangs"]:
+		for lang in val.split(','):
+			langl = lang.lower().lstrip()
+			if(langl in langfreq.keys()):
+				langfreq[langl] = langfreq[langl] + 1
+			else:
+				langfreq[langl] = 1
 
-# List language frequencies of languages appearing more than once
-langfreqc = dict()
-langfreqkey = []
-langfreqindex = dict()
-index = 0
-for lang in langfreq.keys():
-	freq = langfreq[lang]
-	if(freq > 1 and lang != ""):
-		langfreqc[lang] = freq
-		langfreqkey.append(lang)
-		langfreqindex[lang] = index
-		print("{0}: {1}".format(lang, langfreq[lang]))
-		index = index + 1
+	# List language frequencies of languages appearing more than once
+	langfreqc = dict()
+	langfreqkey = []
+	langfreqindex = dict()
+	index = 0
+	for lang in langfreq.keys():
+		freq = langfreq[lang]
+		if(freq > 1 and lang != ""):
+			langfreqc[lang] = freq
+			langfreqkey.append(lang)
+			langfreqindex[lang] = index
+			print("{0}: {1}".format(lang, langfreq[lang]))
+			index = index + 1
 
-print(langfreqc)
+	print(langfreqc)
 
-# Create list of sets for languages
-setLang = []
-for i, val in enumerate(columns["progLangs"]):
-	row = []
-	# Add languages
-	for lang in val.split(','):
-		langl = lang.lower().lstrip()
-		if(langl in langfreqkey):
-			row.append(langl)
-	# Add OS
-	if (not oss[i] == "-"):
-		row.append(oss[i])
-	# Add if all data good
-	if(len(row) > 0):
-		setLang.append(row)
+	# Create list of sets for languages
+	setLang = []
+	for i, val in enumerate(columns["progLangs"]):
+		row = []
+		# Add languages
+		for lang in val.split(','):
+			langl = lang.lower().lstrip()
+			if(langl in langfreqkey):
+				row.append(langl)
+		# Add OS
+		#if (not oss[i] == "-"):
+		#	row.append(oss[i])
+		# Add if all data good
+		if(len(row) > 0):
+			setLang.append(row)
 
-print(langfreqkey)
-for row in setLang:
-	print(row)
+	print(langfreqkey)
+	for row in setLang:
+		print(row)
 
-apriori.apriori(setLang, 20)
+	apriori.apriori(setLang, 0.1)
 
-# Verification (compute lift)
-A = 0
-B = 0
-AB = 0
-count = float(len(setLang))
-for row in setLang:
-	Abool = "c#" in row
-	Bbool = "win" in row
-	if(Abool):
-		A = A + 1
-	if(Bbool):
-		B = B + 1
-	if(Abool and Bbool):
-		AB = AB + 1
+	# Verification (compute lift)
+	A = 0
+	B = 0
+	AB = 0
+	count = float(len(setLang))
+	for row in setLang:
+		Abool = "c#" in row
+		Bbool = "java" in row
+		if(Abool):
+			A = A + 1
+		if(Bbool):
+			B = B + 1
+		if(Abool and Bbool):
+			AB = AB + 1
 
-lift = (AB / count) / ((A/count) * (B/count))
-print("A: {0}, B: {1}, AB: {2}".format(A, B, AB))
-print("lift(A,B) = {0}".format(lift))
+	ABSup = AB / count
+	lift = (AB / count) / ((A/count) * (B/count))
+	print("A: {0}, B: {1}, AB: {2}".format(A, B, AB))
+	print("AB Confidence: {0}".format(ABSup))
+	print("lift(A,B) = {0}".format(lift))
 
 
 
@@ -166,23 +169,34 @@ print("lift(A,B) = {0}".format(lift))
 #
 ###############################
 
-# Create pairs for clustering (age/eng/prog/uni)
-pairs = []
-for i, val in enumerate(uni):
-	if(val > 0 and prog[i] > 0):
-		pairs.append((val, prog[i]))
+def clustering():
+	# Create pairs for clustering (age/eng/prog/uni)
+	pairs = []
+	for i, val in enumerate(uni):
+		if(val > 0 and prog[i] > 0):
+			pairs.append((val, prog[i]))
 
-# Run k-means
-seeds = [pairs[0], pairs[1], pairs[2]]
-clusters = kmeans.kmeans(pairs, seeds)
+	# Run k-means
+	print(pairs)
+	seeds = [pairs[0], pairs[1], pairs[2]]
+	clusters = kmeans.kmeans(pairs, seeds)
 
-# Show diagram with coloured clusters
-for i in clusters:
-	cluster = clusters[i]
-	xaxis = []
-	yaxis = []
-	for pair in cluster:
-		xaxis.append(pair[0])
-		yaxis.append(pair[1])
-	plt.plot(xaxis, yaxis, "o")
-plt.show()
+	# Show diagram with coloured clusters
+	for i in clusters:
+		cluster = clusters[i]
+		xaxis = []
+		yaxis = []
+		for pair in cluster:
+			xaxis.append(pair[0])
+			yaxis.append(pair[1])
+		print(yaxis)
+		plt.plot(xaxis, yaxis, "o")
+
+	plt.axis([0,12,0,12])
+	plt.xlabel('University years')
+	plt.ylabel('Programming skill')
+	plt.show()
+
+
+frequentPattern()
+clustering()
